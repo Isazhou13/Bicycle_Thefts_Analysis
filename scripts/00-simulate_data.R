9483f8cbf1485e7843ab295f42cd28248946cab9
Isazhou13 <isabella.zhou@mail.utoronto.ca>
2024-09-21 10:51:35 -0400

    Sketches of a potential dataset and graph related to Bicycle Thefts

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,12 +1,10 @@
 #### Preamble ####
 # Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Xuanle Zhou 
+# Date: 21 September 2024 
+# Contact: isabella.zhou@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: N/A
 
 #### Workspace setup ####
 library(tidyverse)
