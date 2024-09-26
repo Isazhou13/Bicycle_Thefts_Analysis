5dad4b554e19eadc323f8169c756f5314382e909
Isazhou13 <isabella.zhou@mail.utoronto.ca>
2024-09-26 17:34:27 -0400

    Cleaned the dataset
    Shorten the column name; Adding Location Type; Defined Day Part

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -12,9 +12,13 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/raw_data.csv")
 
-# Select specific columns and then drop rows with missing values
 cleaned_data <- raw_data %>%
-  select(OCC_YEAR, OCC_MONTH, OCC_DOW, PREMISES_TYPE, BIKE_COST, STATUS) %>%
+  mutate(LOCATION_TYPE = gsub("\\s*\\(.*?\\)", "", LOCATION_TYPE))
+
+
+# Select specific columns and then drop rows with missing values
+cleaned_data <- cleaned_data %>%
+  select(OCC_YEAR, OCC_MONTH, OCC_DOW, OCC_HOUR,LOCATION_TYPE, STATUS) %>%
   filter(OCC_YEAR >= 2014 & OCC_YEAR <= 2022) %>%  
   drop_na()  # Remove rows with NA values
 
@@ -22,20 +26,30 @@ cleaned_data <- raw_data %>%
 #Rename column name
 cleaned_data <- cleaned_data %>%
   rename(
-    `Year Offence Occurred` = `OCC_YEAR`,
-    `Month Offence Occurred` = `OCC_MONTH`,
-    `Day of the Week Offence Occurred` = `OCC_DOW`,
-    `Premises Type of Offence` = `PREMISES_TYPE` ,
-    `Cost of Bicycle` = `BIKE_COST`, 
-    `Status of Bicycle` = `STATUS`
+    `Year` = `OCC_YEAR`,
+    `Month` = `OCC_MONTH`,
+    `Day of the Week` = `OCC_DOW`,
+    `Hour` = `OCC_HOUR`,
+    `Location Type` = `LOCATION_TYPE`,
+    `Status` = `STATUS`
+  ) %>%
+  mutate(
+    `Day Part` = case_when(
+      `Hour` >= 6 & `Hour` < 12 ~ "Morning",
+      `Hour` >= 12 & `Hour` < 18 ~ "Afternoon",
+      `Hour` >= 18 & `Hour` < 24 ~ "Evening",
+      TRUE ~ "Overnight"  # Covers hours from 9 PM to 4 AM
+    )
   )
+
 
 # For Status of Bicycle column, capitalize the first letter and make the rest letters in lowercase
 capitalize_first <- function(x) {
   return(toupper(substring(x, 1, 1)) %>% paste0(tolower(substring(x, 2))))
 }
 cleaned_data <- cleaned_data %>%
-  mutate(`Status of Bicycle` = sapply(`Status of Bicycle`, capitalize_first))
+  mutate(`Status` = sapply(`Status`, capitalize_first))
+
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
